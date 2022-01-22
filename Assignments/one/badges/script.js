a0f3bacb040a23c0108ed0bd00a3b6d0ec0d4a43
Hamdrive <hamza.husein017@gmail.com>
2022-01-22 13:04:36 +0530

    [fix] simplify toggle functionality

--- Assignments/one/badges/script.js
+++ Assignments/one/badges/script.js
@@ -2,7 +2,5 @@ const toggleBtn = document.querySelector(".button_badge-toggle");
 const toggleBadge = toggleBtn.querySelector("span");
 
 toggleBtn.addEventListener("click", () => {
-  let selector = toggleBadge.classList;
-  if (selector.contains("toggle")) selector.remove("toggle");
-  else selector.add("toggle");
+  toggleBadge.classList.toggle("toggle");
 });
